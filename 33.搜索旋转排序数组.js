fe3ee2e992d6123919706d23db8075d58f71417d
xuyuan <xy@yuxisoft.cn>
2022-01-28 14:51:47 +0800

    :necktie:init

---
+++ 33.搜索旋转排序数组.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    return nums.findIndex(x => x === target)
+
+};
+// @lc code=end
+
